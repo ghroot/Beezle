f4a36b6cd9820af0ca4166a3d06eec423e6fc2bc
Marcus <marcus.lagerstrom@gmail.com>
2011-11-29 22:33:38 +0000

    Updated cocos2d to 2.0-beta.

---
+++ Beezle/libs/CocosDenshion/TestsAndDemos/FadeToGrey/Classes/FadeToGreyAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  FadeToGreyAppDelegate.h
+//  FadeToGrey
+//
+//  Created by Stephen Oldmeadow on 5/03/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FadeToGreyAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow *window_;
+	UIViewController *viewController_;
+}
+
+@end
