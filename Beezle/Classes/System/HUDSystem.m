dc3ddfd187a4dd394cc51666dbf89a46cf9a4b03
Marcus Lagerstrom <marcus.lagerstrom@gmail.com>
2012-03-01 00:08:11 +0000

    Added a bunch of resources and empty HUDSystem.

---
+++ Beezle/Classes/System/HUDSystem.m
@@ -0,0 +1,13 @@
+//
+//  HUDSystem.m
+//  Beezle
+//
+//  Created by KM Lagerstrom on 29/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HUDSystem.h"
+
+@implementation HUDSystem
+
+@end
