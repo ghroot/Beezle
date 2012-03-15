d99e80f17a36abc23a0dac640e2e810713311666
Marcus Lagerstrom <marcus.lagerstrom@gmail.com>
2012-03-15 22:42:36 +0000

    Show 'e' next to edited levels.

--- Beezle/Classes/State/LevelSelectMenuItem.m
+++ Beezle/Classes/State/LevelSelectMenuItem.m
@@ -7,6 +7,8 @@
 //
 
 #import "LevelSelectMenuItem.h"
+#import "LevelLayout.h"
+#import "LevelLayoutCache.h"
 
 @implementation LevelSelectMenuItem
 
@@ -20,6 +22,15 @@
 -(id) initWithLevelName:(NSString *)levelName target:(id)target selector:(SEL)selector
 {
 	NSString *shortLevelName = [levelName stringByReplacingOccurrencesOfString:@"Level-" withString:@""];
+	if (CONFIG_CAN_EDIT_LEVELS)
+	{
+		LevelLayout *levelLayout = [[LevelLayoutCache sharedLevelLayoutCache] levelLayoutByName:levelName];
+		if (levelLayout != nil &&
+			[levelLayout isEdited])
+		{
+			shortLevelName = [shortLevelName stringByAppendingString:@"e"];
+		}
+	}
 	if (self = [super initWithString:shortLevelName target:target selector:selector])
 	{
 		_levelName = [levelName copy];
