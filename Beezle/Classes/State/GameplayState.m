3c0dafebefd8907343e7ec53ffeecfd236f6aa90
Marcus Lagerstrom <marcus.lagerstrom@gmail.com>
2012-07-30 16:49:29 +0100

    Updated pause image.

--- Beezle/Classes/State/GameplayState.m
+++ Beezle/Classes/State/GameplayState.m
@@ -171,10 +171,10 @@
     _uiLayer = [CCLayer node];
     [self addChild:_uiLayer];
     
-	_pauseMenuItem = [[CCMenuItemImage itemWithNormalImage:@"PauseArrow-01.png" selectedImage:@"PauseArrow-01.png" target:self selector:@selector(pauseGame:)] retain];
+	_pauseMenuItem = [[CCMenuItemImage itemWithNormalImage:@"Syst-GamePause-White.png" selectedImage:@"Syst-GamePause-White.png" target:self selector:@selector(pauseGame:)] retain];
 	CGSize winSize = [[CCDirector sharedDirector] winSize];
-    [_pauseMenuItem setPosition:CGPointMake(winSize.width - 2.0f, winSize.height - 2.0f)];
-    [_pauseMenuItem setAnchorPoint:CGPointMake(1.0f, 1.0f)];
+    [_pauseMenuItem setPosition:CGPointMake(2.0f, winSize.height - 2.0f)];
+    [_pauseMenuItem setAnchorPoint:CGPointMake(0.0f, 1.0f)];
     CCMenu *menu = [CCMenu menuWithItems:_pauseMenuItem, nil];
     [menu setPosition:CGPointZero];
     [_uiLayer addChild:menu];
