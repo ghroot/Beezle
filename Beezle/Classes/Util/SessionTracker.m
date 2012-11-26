856b40eea2d7c8e9afeb377c6fe73788459b9698
Marcus Lagerstrom <marcus.lagerstrom@gmail.com>
2012-11-26 21:01:19 +0000

    Added google analytics tracking.

--- Beezle/Classes/Util/SessionTracker.m
+++ Beezle/Classes/Util/SessionTracker.m
@@ -9,9 +9,11 @@
 #import "SessionTracker.h"
 #import "TestFlight.h"
 #import "Flurry.h"
+#import "GAI.h"
 
 static NSString *FLURRY_ANALYTICS_TOKEN = @"R5JM67WQ5K522SHSW925";
 static NSString *TEST_FLIGHT_TOKEN = @"ae396bc6dbee18a35a1087713acb890b_ODQ5NTEyMDEyLTA0LTI2IDE5OjE0OjM3LjYyMDc3NQ";
+static NSString *GOOGLE_ANALYTICS_TRACKING_ID = @"UA-36586479-1";
 
 void uncaughtExceptionHandler(NSException *exception)
 {
@@ -35,9 +37,19 @@ void uncaughtExceptionHandler(NSException *exception)
 	// Flurry
 	NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
     [Flurry startSession:FLURRY_ANALYTICS_TOKEN];
-	
+
+#ifdef DEBUG
 	// TestFlight
 	[TestFlight takeOff:TEST_FLIGHT_TOKEN];
+#endif
+
+	// Google analytics
+	[[GAI sharedInstance] setTrackUncaughtExceptions:TRUE];
+	[[GAI sharedInstance] setDispatchInterval:20];
+#ifdef DEBUG
+	[[GAI sharedInstance] setDebug:TRUE];
+#endif
+	[[GAI sharedInstance] trackerWithTrackingId:GOOGLE_ANALYTICS_TRACKING_ID];
 }
 
 -(void) trackStartedLevel:(NSString *)levelName
@@ -45,6 +57,8 @@ void uncaughtExceptionHandler(NSException *exception)
 	NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
 	[parameters setObject:levelName forKey:@"levelName"];
 	[Flurry logEvent:@"LEVEL_STARTED" withParameters:parameters];
+
+	[[[GAI sharedInstance] defaultTracker] trackEventWithCategory:@"level" withAction:@"started" withLabel:levelName withValue:[NSNumber numberWithInt:0]];
 }
 
 -(void) trackCompletedLevel:(NSString *)levelName
@@ -52,6 +66,8 @@ void uncaughtExceptionHandler(NSException *exception)
 	NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
 	[parameters setObject:levelName forKey:@"levelName"];
 	[Flurry logEvent:@"LEVEL_COMPLETED" withParameters:parameters];
+
+	[[[GAI sharedInstance] defaultTracker] trackEventWithCategory:@"level" withAction:@"completed" withLabel:levelName withValue:[NSNumber numberWithInt:0]];
 }
 
 -(void) trackFailedLevel:(NSString *)levelName
@@ -59,6 +75,8 @@ void uncaughtExceptionHandler(NSException *exception)
 	NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
 	[parameters setObject:levelName forKey:@"levelName"];
 	[Flurry logEvent:@"LEVEL_FAILED" withParameters:parameters];
+
+	[[[GAI sharedInstance] defaultTracker] trackEventWithCategory:@"level" withAction:@"failed" withLabel:levelName withValue:[NSNumber numberWithInt:0]];
 }
 
 @end
