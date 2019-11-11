44d791843522470ce7f5030f7afc7b404bbb9ee9
Marcus Lagerström <>
2019-11-11 20:36:30 +0100

    Missed references to removed managers.

--- Beezle/Classes/State/PlayState.m
+++ Beezle/Classes/State/PlayState.m
@@ -16,9 +16,6 @@
 #import "CCMenuItemImageScale.h"
 #import "Utils.h"
 #import "PlayerInformation.h"
-#import "GameCenterManager.h"
-#import "FacebookManager.h"
-#import "FacebookHighscoresState.h"
 #import "LiteUtils.h"
 #import "SessionTracker.h"
 #import "CCBReader.h"
@@ -111,26 +108,7 @@ static int nextBeeIndex = 0;
 		[_menu addChild:appStoreMenuItem];
 #endif
 
-//#ifndef LITE_VERSION
-//		[_soundButton setPosition:CGPointMake(winSize.width / 2 - 30.0f, 40.0f)];
-
-//		_gameCenterMenuItem = [CCMenuItemImageScale itemWithNormalImage:@"GameCentre logo-Idle.png" selectedImage:@"GameCentre logo-Idle.png" block:^(id sender){
-//			[[GameCenterManager sharedManager] showLeaderboards];
-//			[[SessionTracker sharedTracker] trackInteraction:@"button" name:@"game center"];
-//		}];
-//		[_gameCenterMenuItem setPosition:CGPointMake(winSize.width / 2 + 30.0f, 40.0f)];
-//		[_menu addChild:_gameCenterMenuItem];
-
-//		CCMenuItemImageScale *facebookMenuItem = [CCMenuItemImageScale itemWithNormalImage:@"Facebook-logo.png" selectedImage:@"Facebook-logo.png" block:^(id sender){
-//			[_game pushState:[FacebookHighscoresState state] transition:FALSE];
-//			[[SessionTracker sharedTracker] trackInteraction:@"button" name:@"facebook"];
-//		}];
-//		[facebookMenuItem setPosition:CGPointMake(80.0f, 40.0f)];
-//		[_menu addChild:facebookMenuItem];
-
-//#else
 		[_soundButton setPosition:CGPointMake(winSize.width / 2, 30.0f)];
-//#endif
 
 #ifdef DEBUG
 		[self createGotoDebugMenu];
